d99a8f8707e290d392d6eec140f4c2b0fb3550e5
Michauxquan <79365450@qq.com>
2016-05-15 16:41:19 +0800

    [F] 智能工厂与二当家后台部分SQL更新

--- CloudSalesDB/Procedures/Manage/M_UpdateClient.sql
+++ CloudSalesDB/Procedures/Manage/M_UpdateClient.sql
@@ -1,20 +1,23 @@
-﻿Use [CloudSales1.0_dev]
+﻿USE [CloudSales1.0_dev]
 GO
 IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'M_UpdateClient')
 BEGIN
 	DROP  Procedure  M_UpdateClient
 END
-
+/****** Object:  StoredProcedure [dbo].[M_UpdateClient]    Script Date: 05/09/2016 14:58:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 /***********************************************************
 过程名称： M_UpdateClient
-功能描述： 编辑客户端
-参数说明：	 
+功能描述： 编辑客户端	
+程序作者： MU 
 编写日期： 2015/10/11
-程序作者： MU
-调试记录： exec M_UpdateClient 
+修改记录： Michaux 2016-05-09
+修改摘要： Logo 为空时不修改
 ************************************************************/
-CREATE PROCEDURE [dbo].M_UpdateClient
+Create PROCEDURE [dbo].[M_UpdateClient]
 @ClientiD nvarchar(64),
 @CompanyName nvarchar(200),
 @MobilePhone nvarchar(64),
@@ -29,9 +32,19 @@ CREATE PROCEDURE [dbo].M_UpdateClient
 AS
 
 --客户端
-update Clients set CompanyName=@CompanyName,
-MobilePhone=@MobilePhone,Industry=@Industry, CityCode=@CityCode, 
-Address=@Address,Description=@Description,ContactName=@ContactName,
-Logo=@Logo,OfficePhone=@OfficePhone
- where ClientiD=@ClientiD
-
+if(isnull(@Logo,'')<>'')
+begin
+	update Clients set CompanyName=@CompanyName,
+	MobilePhone=@MobilePhone,Industry=@Industry, CityCode=@CityCode, 
+	Address=@Address,Description=@Description,ContactName=@ContactName,
+	Logo=@Logo,OfficePhone=@OfficePhone
+	 where ClientiD=@ClientiD
+ end
+else
+begin 
+	update Clients set CompanyName=@CompanyName,
+	MobilePhone=@MobilePhone,Industry=@Industry, CityCode=@CityCode, 
+	Address=@Address,Description=@Description,ContactName=@ContactName,
+	OfficePhone=@OfficePhone
+	 where ClientiD=@ClientiD
+end 
