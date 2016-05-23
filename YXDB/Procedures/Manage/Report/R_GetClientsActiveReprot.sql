99f26c20dd7783a6255cb0bce2d52e123e6bf2b6
Michauxquan <79365450@qq.com>
2016-05-23 16:51:51 +0800

    [F] 智能工厂后台

--- YXDB/Procedures/Manage/Report/R_GetClientsActiveReprot.sql
+++ YXDB/Procedures/Manage/Report/R_GetClientsActiveReprot.sql
@@ -34,12 +34,9 @@ begin
 	set @SqlWhere=' where ReportDate between '''+@BeginTime+''' and dateadd(day,1,'''+@EndTime+''')'
 	if(LEN(@ClientID)>0) 	
 	begin 
-		set @SqlWhere=@SqlWhere+' and ClientID='''+@ClientID+''''
+		set @SqlWhere=@SqlWhere+' and a.ClientID='''+@ClientID+''''
 	 end
-	--按天统计
-	if(@DateType=1)
-	begin	
-		set @SqlText=@SqlText+'select SUM(a.CustomerCount) as CustomerCount,
+	set @SqlText='select SUM(a.CustomerCount) as CustomerCount,
 	  SUM(a.OrdersCount) as OrdersCount,
 	  SUM( a.ActivityCount) as ActivityCount,
 	  SUM( a.ProductCount) as ProductCount,
@@ -51,8 +48,12 @@ begin
 	  SUM( a.TaskCount) as TaskCount ,
 	  SUM( a.DownOrderCount) as DownOrderCount ,
 	  SUM( a.ProductOrderCount) as ProductOrderCount,
-	  sum( a.Vitality) as Vitality	  ,
-	  convert(varchar(8),a.ReportDate,112) AS ReportDate  from M_Report_AgentAction_Day a '
+	  sum( a.Vitality) as Vitality	  '
+	--按天统计 
+	if(@DateType=1)
+	begin	
+	
+	 set @SqlText=@SqlText+', convert(varchar(8),a.ReportDate,112) AS ReportDate  from M_Report_AgentAction_Day a '
 		set @SqlText+=@SqlWhere+'  group by  convert(varchar(8),a.ReportDate,112)'		
 		exec(@SqlText);
 	end
@@ -69,5 +70,7 @@ begin
 		set @SqlText=@SqlText+', convert(varchar(6),a.ReportDate,112) as ReportDate  from M_Report_AgentAction_Day a left join Clients b on a.ClientID=b.ClientID '
 		set @SqlText+=@SqlWhere+' group by   convert(varchar(6),a.ReportDate,112)'		
 		exec(@SqlText);
-	end 
+	end  
+	print @SqlText
+	exec M_Get_Report_AgentActionDayReport @DateType,@BeginTime,@EndTime,''
 end
