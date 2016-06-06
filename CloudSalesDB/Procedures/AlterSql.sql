75809ead0e6afb8c3e40ec0c114b4a531cba514b
sunallen521 <sunallen@163.com>
2016-06-06 18:30:44 +0800

    机会阶段SP

--- CloudSalesDB/Procedures/AlterSql.sql
+++ CloudSalesDB/Procedures/AlterSql.sql
@@ -7,7 +7,7 @@ alter table City  Add [Description] nvarchar(1000) default ''
 GO
 update City set [Description]=Province+' '+City+' '+Counties
 
---产品表
+--处理产品表
 alter table Products add HasDetails int default 0
 --ProdiverID 改 ProviderID，SmallUnitID 改 UnitID
 GO
@@ -30,7 +30,7 @@ select NEWID(),ProductID,'','','','',Price,Price,1,
 					Weight,ProductImage,'','',CreateUserID,getdate(),getdate(),'',ClientID,1 from Products
 					where ProductID not in(select ProductID from ProductDetail where IsDefault=1)
 
---重复执行
+--(重复执行)
 Update p set Remark=REPLACE(Remark,AttrID,AttrName) from ProductDetail p join ProductAttr a on CHARINDEX(a.AttrID, p.Remark)>0
 Update p set Remark=REPLACE(Remark,ValueID,ValueName) from ProductDetail p join AttrValue a on CHARINDEX(a.ValueID, p.Remark)>0
 
@@ -39,19 +39,51 @@ update ProductDetail set Remark=REPLACE(Remark,':','：')
 update ProductDetail set Remark=REPLACE(Remark,',','] [')
 
 
---客户阶段状态
-alter table Customer add StageStatus int default 1
-update Customer set StageStatus=1
-update Customer set StageStatus=2 where CustomerID in (select CustomerID from Orders)
-update Customer set StageStatus=3 where CustomerID in (select CustomerID from Orders where Status>=2)
-
---机会表
+--处理机会表
 alter table Opportunity add TypeID nvarchar(64) default ''
 alter table Opportunity add OrderCode nvarchar(50) default ''
 
 alter table OpportunityProduct add CreateUserID nvarchar(64)
 alter table OpportunityProduct add CreateTime datetime default getdate()
 alter table OpportunityProduct add ImgS nvarchar(500) default ''
+
+insert into Opportunity(OpportunityID,OpportunityCode,Status,TypeID,StageID,TotalMoney,CityCode,Address,PostalCode,PersonName,MobileTele,Remark,CustomerID,OwnerID,CreateTime,OrderTime,OrderID,CreateUserID,AgentID,ClientID)
+select NEWID(),OrderCode,1,TypeID,StageID,TotalMoney,CityCode,Address,PostalCode,PersonName,MobileTele,Remark,CustomerID,OwnerID,CreateTime,OrderTime,OrderID,CreateUserID,AgentID,ClientID from Orders
+
+delete from Orders where Status=0
+
+update opp set Status=2,OrderCode=o.OrderCode from Opportunity opp join Orders o on opp.OrderID=o.OrderID
+update o set OpportunityID=opp.OpportunityID,OpportunityCode=opp.OpportunityCode from Opportunity opp join Orders o on opp.OrderID=o.OrderID
+update Opportunity set OrderID='',TotalMoney=0 where Status=1
+
+--机会意向产品
+insert into OpportunityProduct(OpportunityID,ProductDetailID,ProductID,UnitID,IsBigUnit,Quantity,Price,TotalMoney,Remark,ClientID)
+select  OpportunityID,d.ProductDetailID,d.ProductID,d.UnitID,d.IsBigUnit,d.Quantity,d.Price,d.TotalMoney,d.Remark,o.ClientID from Opportunity o join OrderDetail d on o.OrderID=d.OrderID
+
+
+--机会阶段排序
+Update OpportunityStage set Sort=1 where Mark=1
+
+update o set Sort=2 from OpportunityStage o join
+(select min(Probability) Probability,ClientID from OpportunityStage
+where Status=1 and Sort=0  group by ClientID ) op on o.ClientID=op.ClientID and o.Probability=op.Probability
+
+update o set Sort=3 from OpportunityStage o join
+(select min(Probability) Probability,ClientID from OpportunityStage
+where Status=1 and Sort=0  group by ClientID ) op on o.ClientID=op.ClientID and o.Probability=op.Probability
+
+update o set Sort=4 from OpportunityStage o join
+(select min(Probability) Probability,ClientID from OpportunityStage
+where Status=1 and Sort=0  group by ClientID ) op on o.ClientID=op.ClientID and o.Probability=op.Probability
+
+
+--客户阶段状态
+alter table Customer add StageStatus int default 1
+update Customer set StageStatus=1
+update Customer set StageStatus=2 where CustomerID in (select CustomerID from Orders)
+update Customer set StageStatus=3 where CustomerID in (select CustomerID from Orders where Status>=2)
+
+
 
 --订单表
 alter table Orders add OpportunityID nvarchar(64) default ''
@@ -83,21 +115,6 @@ alter table StorageDetail add DetailsCode nvarchar(100) default ''
 alter table StorageDetail add ProductImage nvarchar(200) default ''
 alter table StorageDetail add ProviderID nvarchar(64) default ''
 alter table StorageDetail add ProviderName nvarchar(100) default ''
-
---处理老数据机会
-insert into Opportunity(OpportunityID,OpportunityCode,Status,TypeID,StageID,TotalMoney,CityCode,Address,PostalCode,PersonName,MobileTele,Remark,CustomerID,OwnerID,CreateTime,OrderTime,OrderID,CreateUserID,AgentID,ClientID)
-select NEWID(),OrderCode,1,TypeID,StageID,TotalMoney,CityCode,Address,PostalCode,PersonName,MobileTele,Remark,CustomerID,OwnerID,CreateTime,OrderTime,OrderID,CreateUserID,AgentID,ClientID from Orders
-
-delete from Orders where Status=0
-
-update opp set Status=2,OrderCode=o.OrderCode from Opportunity opp join Orders o on opp.OrderID=o.OrderID
-update o set OpportunityID=opp.OpportunityID,OpportunityCode=opp.OpportunityCode from Opportunity opp join Orders o on opp.OrderID=o.OrderID
-
-update Opportunity set OrderID='',TotalMoney=0 where Status=1
-
---机会意向产品
-insert into OpportunityProduct(OpportunityID,ProductDetailID,ProductID,UnitID,IsBigUnit,Quantity,Price,TotalMoney,Remark,ClientID)
-select  OpportunityID,d.ProductDetailID,d.ProductID,d.UnitID,d.IsBigUnit,d.Quantity,d.Price,d.TotalMoney,d.Remark,o.ClientID from Opportunity o join OrderDetail d on o.OrderID=d.OrderID
 
 
 
--- CloudSalesDB/Procedures/System/P_DeleteOpportunityStage.sql
+++ CloudSalesDB/Procedures/System/P_DeleteOpportunityStage.sql
@@ -10,7 +10,7 @@ GO
 过程名称： P_DeleteOpportunityStage
 功能描述： 删除机会阶段
 参数说明：	 
-编写日期： 2015/12/5
+编写日期： 2016/6/6
 程序作者： Allen
 调试记录： exec P_DeleteOpportunityStage 
 ************************************************************/
@@ -23,25 +23,21 @@ AS
 begin tran
 
 --阶段下有机会
-if exists(select AutoID from Orders where StageID=@StageID and Status=0 and ClientID=@ClientID)
+if exists(select AutoID from Opportunity where StageID=@StageID and Status<>9 and ClientID=@ClientID)
 begin
 	rollback tran
 	return
 end
 
-declare @Err int=0,@Probability decimal(18,4)=0,@Mark int=0,@Status int=1,@PrevStageID nvarchar(64)
+declare @Err int=0,@Sort int=0,@Mark int=0,@Status int=1,@PrevStageID nvarchar(64)
 
-select @Probability=Probability,@Mark=Mark,@Status=Status from OpportunityStage where StageID=@StageID and ClientID=@ClientID
+select @Sort=Sort,@Mark=Mark,@Status=Status from OpportunityStage where StageID=@StageID and ClientID=@ClientID
 if(@Mark=0 and @Status=1)
 begin
 
-	update OpportunityStage set Status=9 where StageID=@StageID and ClientID=@ClientID
+	update  OpportunityStage set Status=9 where StageID=@StageID and ClientID=@ClientID 
 
-	--取得上个机会阶段
-	select top 1 @PrevStageID=StageID from OpportunityStage where ClientID=@ClientID and Probability < @Probability and Status<>9
-
-	--更改机会阶段
-	update Orders set StageID = @PrevStageID where ClientID=@ClientID and StageID=@StageID
+	update  OpportunityStage set Sort=Sort-1 where  ClientID=@ClientID and Sort>@Sort
 
 	set @Err+=@@error
 end
