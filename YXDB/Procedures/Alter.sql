4d0d2d507fcc2123132e4060ab74ecdfd1f96b37
sunallen521 <sunallen@163.com>
2016-06-20 11:22:43 +0800

    SQL 语句

--- YXDB/Procedures/Alter.sql
+++ YXDB/Procedures/Alter.sql
@@ -1,1 +1,16 @@
 ﻿
+--处理客户需求单和订单数
+alter table Customer add DemandCount int default 0
+alter table Customer add DYCount int default 0
+alter table Customer add DHCount int default 0
+
+update Customer set DemandCount=0,DYCount=0,DHCount=0
+
+update C set DemandCount=o.Quantity from Customer c join
+(select CustomerID,COUNT(AutoID) Quantity from Orders where OrderStatus=0 group by CustomerID) o on c.CustomerID=o.CustomerID
+
+update C set DYCount=o.Quantity from Customer c join
+(select CustomerID,COUNT(AutoID) Quantity from Orders where OrderStatus between 1 and 2 and OrderType=1 group by CustomerID) o on c.CustomerID=o.CustomerID
+
+update C set DHCount=o.Quantity from Customer c join
+(select CustomerID,COUNT(AutoID) Quantity from Orders where OrderStatus between 1 and 2 and OrderType=2 group by CustomerID) o on c.CustomerID=o.CustomerID
