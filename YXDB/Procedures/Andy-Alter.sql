1960962604be19b723cb865f133471dcdd4ed0c0
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-06-23 19:36:40 +0800

    调整

--- YXDB/Procedures/Andy-Alter.sql
+++ YXDB/Procedures/Andy-Alter.sql
@@ -1,4 +1,62 @@
 ﻿
 
 use IntFactory
-ALTER TABLE [dbo].[PlateMaking] ADD Type int  DEFAULT (1) not null
+
+
+insert into Menu
+values('102019035','打样报价','','Orders','FentOrderReport','','',1,0,'102010500',1035,4,0,1,'')
+
+
+CREATE TABLE [dbo].[TaskReplyAttachmentRelation](
+	[AutoID] [int] IDENTITY(1,1) NOT NULL,
+	[TaskID] [nvarchar](64) NOT NULL,
+	[ReplyID] [nvarchar](64) NULL,
+	[AttachmentID] [nvarchar](64) NOT NULL,
+	[Status] [int] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[TaskReplyAttachmentRelation] ADD  CONSTRAINT [DF_TaskReplyAttachmentRelation_Status]  DEFAULT ((1)) FOR [Status]
+GO
+
+
+CREATE TABLE [dbo].[CustomerReplyAttachmentRelation](
+	[AutoID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerID] [nvarchar](64) NOT NULL,
+	[ReplyID] [nvarchar](64) NOT NULL,
+	[AttachmentID] [nvarchar](64) NOT NULL,
+	[Status] [int] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[CustomerReplyAttachmentRelation] ADD  CONSTRAINT [DF_CustomerReplyAttachmentRelation_Status]  DEFAULT ((1)) FOR [Status]
+GO
+
+CREATE TABLE [dbo].[Attachment](
+	[AutoID] [int] IDENTITY(1,1) NOT NULL,
+	[AttachmentID] [nvarchar](64) NOT NULL,
+	[Type] [int] NOT NULL,
+	[ServerUrl] [nvarchar](200) NULL,
+	[FilePath] [nvarchar](200) NOT NULL,
+	[FileName] [nvarchar](200) NOT NULL,
+	[OriginalName] [nvarchar](200) NOT NULL,
+	[ThumbnailName] [nvarchar](200) NULL,
+	[Size] [bigint] NOT NULL,
+	[CreateUserID] [nvarchar](64) NULL,
+	[CreateTime] [datetime] NOT NULL,
+	[UpdateTime] [datetime] NULL,
+	[ClientID] [nvarchar](64) NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Attachment] ADD  CONSTRAINT [DF_TaskReplyAttachment_Type]  DEFAULT ((1)) FOR [Type]
+GO
+
+ALTER TABLE [dbo].[Attachment] ADD  CONSTRAINT [DF_Attachment_Size]  DEFAULT ((0)) FOR [Size]
+GO
+
+ALTER TABLE [dbo].[Attachment] ADD  CONSTRAINT [DF_TaskReplyAttachment_CreateTime]  DEFAULT (getdate()) FOR [CreateTime]
+GO
--- YXDB/Procedures/System/P_DeleteOrderProcess.sql
+++ YXDB/Procedures/System/P_DeleteOrderProcess.sql
@@ -16,13 +16,15 @@ GO
 ************************************************************/
 CREATE PROCEDURE [dbo].P_DeleteOrderProcess
 @ProcessID nvarchar(64),
-@ClientID nvarchar(64)=''
+@ClientID nvarchar(64)='',
+@Result int output
 AS
 
 begin tran
 
 if exists(select AutoID from Orders where ProcessID=@ProcessID and Status<>9)
 begin
+	set @Result=2
 	rollback tran
 	return
 end
@@ -32,9 +34,11 @@ declare @Err int=0
 Update OrderProcess set Status=9 where ProcessID=@ProcessID
 
 set @Err+=@@error
+set @Result=1
 
 if(@Err>0)
 begin
+	set @Result=0
 	rollback tran
 end 
 else
