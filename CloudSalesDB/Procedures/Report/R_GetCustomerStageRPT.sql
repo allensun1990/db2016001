6be6631ca0301625391d2ad9af51878a3e95c4f7
sunallen521 <sunallen@163.com>
2016-06-28 10:05:54 +0800

    机会统计SP

--- CloudSalesDB/Procedures/Report/R_GetCustomerStageRPT.sql
+++ CloudSalesDB/Procedures/Report/R_GetCustomerStageRPT.sql
@@ -12,7 +12,7 @@ GO
 参数说明：	 
 编写日期： 2016/06/13
 程序作者： Michaux
-调试记录： exec R_GetCustomerStageRPT '','','8c9b5e24-2bb5-4d87-9a5a-b1aa4c5b81f8','eda082bc-b848-4de8-8776-70235424fc06'
+调试记录： exec R_GetCustomerStageRPT 'a323cd22-e07a-4d07-8ff9-178ffc04e6b6','','',1
 ************************************************************/
 
 create proc [dbo].[R_GetCustomerStageRPT]
@@ -22,9 +22,18 @@ create proc [dbo].[R_GetCustomerStageRPT]
 @Type int=0
 as
 
+if(@BeginTime='')
+begin
+	set @BeginTime='1900-1-1'
+end
+
 if(@EndTime<>'')
 begin
-set @EndTime= @EndTime+' 23:59:59'
+	set @EndTime= @EndTime+' 23:59:59'
+end
+else
+begin
+	set @EndTime=getdate() 
 end
 
 if(@Type=1)
@@ -35,20 +44,19 @@ begin
 	union
 	select COUNT(StageStatus) as Value,3 as StageStatus  from Customer where OrderTIme>@BeginTime and OrderTime<@EndTime  and ClientID=@ClientID
 
-	select COUNT(1) value ,1 as StageStatus,b.SourceCode,b.SourceName from  Customer a  left  join CustomSource  b on a.SourceID=b.SourceID 
-	where a.Status<>9  and b.Status<>9  and  a.CreateTime>@BeginTime and a.CreateTime<@EndTime and a.ClientID=@ClientID
-	group by b.SourceCode,b.SourceName
+	select COUNT(0) value ,1 as StageStatus,SourceID,'' SourceName from  Customer 
+	where Status<>9  and ClientID=@ClientID
+	group by SourceID
 	union
-	select COUNT(1) value ,2 as StageStatus,b.StageID as SourceCode,c.StageName as SourceName from  Customer a 
-	 left  join Opportunity  b on a.CustomerID=b.CustomerID  
-	 left join OpportunityStage c on b.StageID=c.StageID
-	  and b.OpportunityID=a.OpportunityID
+	select COUNT(1) value ,2 as StageStatus,b.StageID as SourceID,c.StageName as SourceName from  Customer a 
+	left join Opportunity  b on a.CustomerID=b.CustomerID  
+	left join OpportunityStage c on b.StageID=c.StageID and b.OpportunityID=a.OpportunityID
 	where a.Status<>9  and b.Status<>9 and c.Status<>9 and a.OpportunityTime>@BeginTime and a.OpportunityTime<@EndTime  and a.ClientID=@ClientID
 	group by b.StageID ,c.StageName 
 	union
-	select COUNT(1) value ,3 as StageStatus ,'' SourceCode,'' as SourceName from  Customer a 
-	 left  join Orders  b on a.CustomerID=b.CustomerID    and a.OrderID=b.OrderID
-	where a.Status<>9  and b.Status<>9     and a.OrderTIme>@BeginTime and a.OrderTime<@EndTime  and a.ClientID=@ClientID
+	select COUNT(1) value ,3 as StageStatus ,'' SourceID,'' as SourceName from  Customer a 
+	join Orders  b on a.CustomerID=b.CustomerID   and a.OrderID=b.OrderID
+	where a.Status<>9  and b.Status<>9   and a.OrderTIme>@BeginTime and a.OrderTime<@EndTime  and a.ClientID=@ClientID
 end
 else
 begin
@@ -58,21 +66,19 @@ begin
 	union
 	select COUNT(StageStatus) as Value,3 as StageStatus  from Customer where CreateTime>@BeginTime and CreateTime<@EndTime and OrderTime>@BeginTime and OrderTime<@EndTime  and ClientID=@ClientID
 
-	select COUNT(1) value ,1 as StageStatus,b.SourceCode,b.SourceName from  Customer a  left  join CustomSource  b on a.SourceID=b.SourceID 
-	where a.Status<>9  and b.Status<>9  and  a.CreateTime>@BeginTime and a.CreateTime<@EndTime and a.ClientID=@ClientID
-	group by b.SourceCode,b.SourceName,StageStatus
+	select COUNT(1) value ,1 as StageStatus,SourceID,'' SourceName from  Customer
+	where Status<>9  and  CreateTime>@BeginTime and CreateTime<@EndTime and ClientID=@ClientID
+	group by SourceID
 	union
-	select COUNT(1) value ,2 as StageStatus,b.StageID as SourceCode,c.StageName as SourceName from  Customer a 
-	 left  join Opportunity  b on a.CustomerID=b.CustomerID  
-	 left join OpportunityStage c on b.StageID=c.StageID
-	  and b.OpportunityID=a.OpportunityID
+	select COUNT(1) value ,2 as StageStatus,b.StageID as SourceID,c.StageName as SourceName from  Customer a 
+	left join Opportunity  b on a.CustomerID=b.CustomerID  
+	left join OpportunityStage c on b.StageID=c.StageID and b.OpportunityID=a.OpportunityID
 	where a.Status<>9  and b.Status<>9 and c.Status<>9 and a.CreateTime>@BeginTime and a.CreateTime<@EndTime and a.OpportunityTime>@BeginTime and a.OpportunityTime<@EndTime  and a.ClientID=@ClientID
 	group by b.StageID ,c.StageName 
 	union
-	select COUNT(1) value ,3 as StageStatus ,'' SourceCode,'' as SourceName from  Customer a 
-	 left  join Orders  b on a.CustomerID=b.CustomerID   and a.OrderID=b.OrderID
+	select COUNT(1) value ,3 as StageStatus ,'' SourceID,'' as SourceName from  Customer a 
+	join Orders  b on a.CustomerID=b.CustomerID   and a.OrderID=b.OrderID
 	where a.Status<>9  and b.Status<>9  and a.CreateTime>@BeginTime and a.CreateTime<@EndTime  and a.OrderTIme>@BeginTime and a.OrderTime<@EndTime  and a.ClientID=@ClientID
-
 end
 
 
