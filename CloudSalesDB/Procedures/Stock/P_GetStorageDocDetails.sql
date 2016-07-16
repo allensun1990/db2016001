d6c5cb5e614f416e1ae64629541cad667e77e63f
sunallen521 <sunallen@163.com>
2016-07-16 21:21:17 +0800

    采购单入库明细

---
+++ CloudSalesDB/Procedures/Stock/P_GetStorageDocDetails.sql
@@ -0,0 +1,29 @@
+﻿Use [CloudSales1.0_dev]
+GO
+IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'P_GetStorageDocDetails')
+BEGIN
+	DROP  Procedure  P_GetStorageDocDetails
+END
+
+GO
+/***********************************************************
+过程名称： P_GetStorageDocDetails
+功能描述： 获取单据明细列表
+参数说明：	 
+编写日期： 2016/7/16
+程序作者： Allen
+调试记录： exec P_GetStorageDocDetails '25A3D084-7CE9-42ED-8BA6-5DDC26B30569'
+			
+			
+************************************************************/
+CREATE PROCEDURE [dbo].[P_GetStorageDocDetails]
+	@DocID nvarchar(64)=''
+AS
+
+	select DocID into #TempDoc from StorageDocPart where OriginalID=@DocID
+
+	select DocID,DocCode,CreateTime,CreateUserID from StorageDocPart where DocID in (select DocID from #TempDoc) order by CreateTime desc
+
+	select * from StoragePartDetail where DocID in (select DocID from #TempDoc)
+ 
+
