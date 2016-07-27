206fea351e0cde35f5dec3081ff8e988c4c80f00
sunallen521 <sunallen@163.com>
2016-07-27 15:04:08 +0800

    删除无效SP

--- YXDB/Procedures/ZNGCAlter.sql
+++ YXDB/Procedures/ZNGCAlter.sql
@@ -16,6 +16,25 @@ Go
 DROP  Procedure  P_AddShoppingCartBatchOut
 GO
 DROP  Procedure  P_GetOpportunitys
+GO
+DROP  Procedure  P_GetActivitys
+GO
+DROP  Procedure  P_InsertCustomSource
+GO
+DROP  Procedure  P_InsertCustomStage
+GO
+DROP  Procedure  P_DeletetCustomStage
+GO
+DROP  Procedure  P_UpdateCustomerStage
+
+--删除订单类别表
+drop table OrderType
+--删除客户来源表
+drop table CustomSource
+drop table Activity
+drop table ActivityReply
+drop table CustomerStageLog
+
 
 --删除无效菜单和权限
 delete from Menu where IsHide=1 
