2a575e4c3b3a0115eac5e790deaa9d115c66ffdc
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-08-01 11:33:44 +0800

    优化

--- YXDB/Procedures/Andy-Alter.sql
+++ YXDB/Procedures/Andy-Alter.sql
@@ -1,6 +1,4 @@
 ﻿
-
-
 insert into Menu
 select '108020900','标签设置',Area,Controller,'LabelSet',IcoPath,IcoHover,Type,IsHide,PCode,8,Layer,IsMenu,IsLimit,Remark from Menu
 where AutoID=183
@@ -9,14 +7,12 @@ alter table orders add YXOrderID nvarchar(64) null
 
 USE [IntFactory]
 GO
-
 /****** Object:  Table [dbo].[OrderPriceRange]    Script Date: 07/13/2016 15:50:39 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE TABLE [dbo].[OrderPriceRange](
 	[AutoID] [int] IDENTITY(1,1) NOT NULL,
 	[RangeID] [nvarchar](64) NOT NULL,
@@ -29,7 +25,6 @@ CREATE TABLE [dbo].[OrderPriceRange](
 	[CreateTime] [datetime] NOT NULL,
 	[CreateUserID] [nvarchar](64) NULL
 ) ON [PRIMARY]
-
 GO
 
 ALTER TABLE [dbo].[OrderPriceRange] ADD  CONSTRAINT [DF_OrderPriceRange_MinQuantity]  DEFAULT ((1)) FOR [MinQuantity]
@@ -40,8 +35,6 @@ GO
 
 ALTER TABLE [dbo].[OrderPriceRange] ADD  CONSTRAINT [DF_OrderPriceRange_CreateTime]  DEFAULT (getdate()) FOR [CreateTime]
 GO
-
-alter table users add WeiXinID nvarchar(200) null
 
 
 
