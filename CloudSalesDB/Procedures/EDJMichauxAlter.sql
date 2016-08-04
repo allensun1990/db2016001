2e73fa9cd8979fc3a9bfb4c9fcf2b0aa306197a4
Michaux <79365450@qq.com>
2016-08-04 15:39:46 +0800

    [F]1

--- CloudSalesDB/Procedures/EDJMichauxAlter.sql
+++ CloudSalesDB/Procedures/EDJMichauxAlter.sql
@@ -12,6 +12,7 @@ create table ClientMemberLevel(
 [Origin] int,
 [ImgUrl] [nvarchar](248) Not NULL,
 )
+go
 
 create table IntegerFeeChange
 (
@@ -26,18 +27,49 @@ AgentID varchar(50) not null,
 ClientID varchar(50) not null,
 Reamrk varchar(500) 
 )
-
+go
 
 alter table Customer Add IntegerFee decimal(18,4) default(0.00)
+go
 alter table Customer Add TotalIntegerFee decimal(18,4) default(0.00)
+go
 alter table Customer add MemberLevelID varchar(50) 
-
+go
 alter table  M_Report_AgentAction_Day add UserNum int default 1
+go
 alter table  M_Report_AgentAction_Day add Vitality decimal(18,4) default 0.0000
- 
+ go
 update Customer set IntegerFee=0.00,TotalIntegerFee=0.000 
+go
 
-/*新增存储过程*/
+/*配置已存在的客户的客户等级*/
+
+create table #memberLevel (Name varchar(50),integFeeMore decimal(18,2),DiscountFee decimal(18,2) ,origin int )
+insert into #memberLevel values('普通会员',0,1.00,1)
+insert into #memberLevel values('青铜会员',1000,0.98,2)
+insert into #memberLevel values('白银会员',5000,0.96,3)
+insert into #memberLevel values('黄金会员',9000,0.92,4)
+ 
+ insert into clientMemberLevel 
+ select newid(),b.Name,b.integFeeMore,b.DiscountFee,1,'',GETDATE(),a.AgentID,a.ClientID,b.origin,'' from  Clients a join #memberLevel b on 1=1 where status<>9
+ 
+ drop table #memberLevel
+
+ /*刷新人数和活跃度*/
+  update M_Report_AgentAction_Day set UserNum=UserCount,Vitality=
+cast( 
+	round(
+		(CustomerCount+OrdersCOunt+ ActivityCount+ProductCount+UsersCount+AgentCount+OpportunityCount+PurchaseCount+WarehousingCount+ProductCount)
+		/ cast(UserNum as decimal(18,4)
+	 ),4) as  decimal(18,4)
+ )  
+ from 
+ (
+	select isnull(Count(ClientID),0) as UserCount ,Users.ClientID from Users where Status=1  group by Users.ClientID
+) a 
+join  M_Report_AgentAction_Day   on a.ClientID=M_Report_AgentAction_Day.ClientID
+  
+  /*新增存储过程*/
 P_InsertClientMemberLevel
 P_UpdateClientMemberLevel
 P_DeleteClientMemberLevel
@@ -69,37 +101,9 @@ M_DeleteRole
 Rpt_AgentAction_Day
 M_GetClientOrders 
 P_GetUserToLogin
-/*配置已存在的客户的客户等级*/
-
-create table #memberLevel (Name varchar(50),integFeeMore decimal(18,2),DiscountFee decimal(18,2) ,origin int )
-insert into #memberLevel values('普通会员',0,1.00,1)
-insert into #memberLevel values('青铜会员',1000,0.98,2)
-insert into #memberLevel values('白银会员',5000,0.96,3)
-insert into #memberLevel values('黄金会员',9000,0.92,4)
- 
- insert into clientMemberLevel 
- select newid(),b.Name,b.integFeeMore,b.DiscountFee,1,'',GETDATE(),a.AgentID,a.ClientID,b.origin,'' from  Clients a join #memberLevel b on 1=1 where status<>9
- 
- drop table #memberLevel
-
- /*刷新人数和活跃度*/
-  update M_Report_AgentAction_Day set UserNum=UserCount,Vitality=
-cast( 
-	round(
-		(CustomerCount+OrdersCOunt+ ActivityCount+ProductCount+UsersCount+AgentCount+OpportunityCount+PurchaseCount+WarehousingCount+ProductCount)
-		/ cast(UserNum as decimal(18,4)
-	 ),4) as  decimal(18,4)
- )  
- from 
- (
-	select isnull(Count(ClientID),0) as UserCount ,Users.ClientID from Users where Status=1  group by Users.ClientID
-) a 
-join  M_Report_AgentAction_Day   on a.ClientID=M_Report_AgentAction_Day.ClientID
-  
 
 
-
-  ----修改
+  ---下次版本更新-修改
   P_ConfirmAgentOrderSend
   P_AuditReturnIn
   P_ConfirmAgentOrderOut
