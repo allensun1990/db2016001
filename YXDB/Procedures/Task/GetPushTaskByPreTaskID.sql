2f01808c1656d45099a85e3a5a85d7ca8c72eb6b
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-08-15 10:53:15 +0800

    优化

---
+++ YXDB/Procedures/Task/GetPushTaskByPreTaskID.sql
@@ -0,0 +1,38 @@
+﻿Use IntFactory
+GO
+IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'P_GetPushTaskByPreTaskID')
+BEGIN
+	DROP  Procedure  P_GetPushTaskByPreTaskID
+END
+
+GO
+/***********************************************************
+过程名称： P_GetPushTaskByPreTaskID
+功能描述： 获取需push任务通过上级TaskID
+参数说明：	 
+编写日期： 2016/8/12
+程序作者： MU
+调试记录：  exec P_GetPushTaskByPreTaskID '2ef3ce07-1e21-4a46-9706-02ade5ccf7c9'
+************************************************************/
+CREATE PROCEDURE [dbo].P_GetPushTaskByPreTaskID
+@TaskID nvarchar(64)
+as
+declare @orderid nvarchar(64),@sort int,
+@pretitle nvarchar(200),@title nvarchar(200),@onwerid nvarchar(64)
+
+select @orderid=OrderID,@pretitle=Title,@sort=Sort from OrderTask
+where TaskID=@TaskID
+
+select @onwerid=OwnerID,@title=Title from OrderTask
+where OrderID=@orderid and Sort=@sort+1 and FinishStatus<>2
+
+if(@onwerid<>'')
+	select ProjectID as OpenID,@pretitle as PreTitle,@title as Title,@onwerid as OnwerID from UserAccounts
+	where UserID=@onwerid and AccountType=4 and ProjectID<>''
+
+		 
+
+
+
+
+
---
+++ YXDB/Procedures/Task/P_GetPushTasksByOrderID.sql
@@ -0,0 +1,33 @@
+﻿Use IntFactory_dev
+GO
+IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'P_GetPushTasksByOrderID')
+BEGIN
+	DROP  Procedure  P_GetPushTasksByOrderID
+END
+
+GO
+/***********************************************************
+过程名称： P_GetPushTasksByOrderID
+功能描述： 获取需push任务列表通过OrderID
+参数说明：	 
+编写日期： 2016/8/12
+程序作者： MU
+调试记录：  exec P_GetPushTasksByOrderID '2ef3ce07-1e21-4a46-9706-02ade5ccf7c9'
+************************************************************/
+CREATE PROCEDURE [dbo].P_GetPushTasksByOrderID
+@OrderID nvarchar(64)
+as
+declare @PlanTime datetime
+select @PlanTime=PlanTime from orders where OrderID=@OrderID
+
+select u.ProjectID as OpenID,t.Title,t.OwnerID,@PlanTime as EndTime  from OrderTask as t,UserAccounts u
+where t.OwnerID=u.UserID and  t.OrderID=@OrderID and
+ u.AccountType=4 and u.ProjectID<>''
+ order by t.sort asc
+
+		 
+
+
+
+
+
