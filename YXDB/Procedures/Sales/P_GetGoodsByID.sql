3843de2925b517f96ca52851440d2f9c0ee52a1b
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-09-02 18:37:20 +0800

    优化

---
+++ YXDB/Procedures/Sales/P_GetGoodsByID.sql
@@ -0,0 +1,24 @@
+﻿Use IntFactory
+GO
+IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'P_GetGoodsByID')
+BEGIN
+	DROP  Procedure  P_GetGoodsByID
+END
+
+GO
+/***********************************************************
+过程名称： P_GetGoodsByID
+功能描述： 获取客户订单详情
+参数说明：	 
+编写日期： 2016/9/2
+程序作者： MU
+调试记录： exec P_GetGoodsByID 'd3c9af49-e47c-4773-b2af-1fd8ccae127d'
+************************************************************/
+CREATE PROCEDURE [dbo].[P_GetGoodsByID]
+	@GoodsID nvarchar(64)='',
+	@ClientID nvarchar(64)=''
+AS
+select * from Goods where GoodsID=@GoodsID and ClientID=@ClientID
+
+
+
