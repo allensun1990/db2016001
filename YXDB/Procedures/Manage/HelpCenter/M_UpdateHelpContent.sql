90a1367a8f2e0e32e9d7bb205acfbb6693e53fc2
song19940906 <2079973693@qq.com>
2016-09-21 20:00:39 +0800

    9.21帮助中心后台-sql语句-联动

---
+++ YXDB/Procedures/Manage/HelpCenter/M_UpdateHelpContent.sql
@@ -0,0 +1,47 @@
+﻿USE [IntFactory]
+GO
+IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'M_UpdateHelpContent')
+BEGIN
+	DROP  Procedure  M_UpdateHelpContent
+END
+/****** Object:  StoredProcedure [dbo].[M_UpdateHelpContent]    Script Date: 05/09/2016 14:58:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/***********************************************************
+过程名称： M_UpdateHelpContent
+功能描述： 编辑帮助中心内容	
+程序作者： allen
+编写日期： 2016/6/6
+************************************************************/
+
+CREATE PROCEDURE [dbo].[M_UpdateHelpContent]
+	@ContentID nvarchar(64),
+	@TypeID nvarchar(64),
+	@Sort nvarchar(64),
+	@Title nvarchar(64),
+	@MainImg nvarchar(200)='',
+	@KeyWords nvarchar(64),
+	@Detail text,
+	@Result int output --1 成功
+AS
+	declare @oldSort int
+
+	select @oldSort=Sort from M_HelpContent where TypeID=@TypeID
+	
+	if(@oldSort<@Sort)
+	begin
+		update M_HelpContent set Sort-=1 where TypeID=@TypeID and ContentID in(select ContentID from M_HelpContent where @oldSort<=Sort and Sort<=@Sort and TypeID=@TypeID) 
+	end
+
+	if(@oldSort>@Sort)
+	begin
+		update M_HelpContent set Sort+=1 where TypeID=@TypeID and ContentID in(select ContentID from M_HelpContent where @oldSort>=Sort and Sort>=@Sort and TypeID=@TypeID)
+	end
+	
+	Update M_HelpContent set Title=@Title,Sort=@Sort,KeyWords=@KeyWords,MainImg=@MainImg,Detail=@Detail,TypeID=@TypeID,UpdateTime=getdate() where contentID=@ContentID
+
+	set @Result=1
+GO
+
---
+++ YXDB/Procedures/Manage/HelpCenter/M_UpdateHelpType.sql
@@ -0,0 +1,53 @@
+﻿USE [IntFactory]
+GO
+IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'M_UpdateHelpType')
+BEGIN
+	DROP  Procedure  M_UpdateHelpType
+END
+/****** Object:  StoredProcedure [dbo].[M_UpdateHelpType]    Script Date: 05/09/2016 14:58:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/***********************************************************
+过程名称： M_UpdateHelpType
+功能描述： 编辑帮助中心分类	
+程序作者： allen
+编写日期： 2016/6/6
+************************************************************/
+
+CREATE PROCEDURE [dbo].[M_UpdateHelpType]
+	@TypeID nvarchar(64),
+	@Name nvarchar(64),
+	@Remark nvarchar(400),
+	@ModuleType int,
+	@Img nvarchar(200)='',	
+	@Sort int,
+	@Result int output--1 成功
+AS
+	declare @oldSort int
+
+	select @oldSort=Sort from M_HelpType where TypeID=@TypeID
+	
+	if(@oldSort<@Sort)
+	begin
+		update M_HelpType set Sort-=1 where ModuleType=@ModuleType and TypeID in(select TypeID from M_HelpType where @oldSort<=Sort and Sort<=@Sort and ModuleType=@ModuleType) 
+	end
+
+	if(@oldSort>@Sort)
+	begin
+		update M_HelpType set Sort+=1 where ModuleType=@ModuleType and TypeID in(select TypeID from M_HelpType where @oldSort>=Sort and Sort>=@Sort and ModuleType=@ModuleType)
+	end
+
+	Update M_HelpType set Name=@Name,Remark=@Remark,Icon=@Img,ModuleType=@ModuleType,Sort=@Sort where TypeID=@TypeID	
+
+
+
+	set @Result=1
+GO
+
+
+
+
+
+
