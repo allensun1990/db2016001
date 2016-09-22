9fcf8f6e47d8e15eebbd8ac6a526586a98359ce7
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-09-22 20:32:45 +0800

    优化

--- YXDB/Procedures/Andy-Alter.sql
+++ YXDB/Procedures/Andy-Alter.sql
@@ -38,6 +38,73 @@ values('206020200','内容列表','','HelpCenter','Contents',
 '','',2,0,
 '206020000',2,3,1,1,'')
 
+CREATE TABLE [dbo].[M_HelpType](
+	[AutoID] [int] IDENTITY(1,1) NOT NULL,
+	[TypeID] [nvarchar](64) NOT NULL,
+	[Name] [nvarchar](100) NULL,
+	[Icon] [nvarchar](200) NULL,
+	[Remark] [nvarchar](400) NULL,
+	[Sort] [int] NULL,
+	[Status] [int] NOT NULL,
+	[ModuleType] [int] NOT NULL,
+	[CreateUserID] [nvarchar](64) NULL,
+	[CreateTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_M_HelpType] PRIMARY KEY CLUSTERED 
+(
+	[TypeID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[M_HelpType] ADD  CONSTRAINT [DF_M_HelpType_Sort]  DEFAULT ((0)) FOR [Sort]
+GO
+
+ALTER TABLE [dbo].[M_HelpType] ADD  CONSTRAINT [DF_M_HelpType_Status]  DEFAULT ((1)) FOR [Status]
+GO
+
+ALTER TABLE [dbo].[M_HelpType] ADD  CONSTRAINT [DF_M_HelpType_ModuleType]  DEFAULT ((1)) FOR [ModuleType]
+GO
+
+ALTER TABLE [dbo].[M_HelpType] ADD  CONSTRAINT [DF_M_HelpType_CreateTime]  DEFAULT (getdate()) FOR [CreateTime]
+GO
+
+CREATE TABLE [dbo].[M_HelpContent](
+	[AutoID] [int] IDENTITY(1,1) NOT NULL,
+	[ContentID] [nvarchar](64) NOT NULL,
+	[TypeID] [nvarchar](64) NOT NULL,
+	[Title] [nvarchar](200) NOT NULL,
+	[MainImg] [nvarchar](200) NULL,
+	[Detail] [text] NOT NULL,
+	[KeyWords] [nvarchar](200) NULL,
+	[Sort] [int] NULL,
+	[Status] [int] NULL,
+	[ClickNumber] [int] NOT NULL,
+	[CreateUserID] [nvarchar](64) NULL,
+	[CreateTime] [datetime] NULL,
+	[UpdateTime] [datetime] NULL,
+ CONSTRAINT [PK_M_HelpContent] PRIMARY KEY CLUSTERED 
+(
+	[ContentID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[M_HelpContent] ADD  CONSTRAINT [DF_M_HelpContent_Sort]  DEFAULT ((0)) FOR [Sort]
+GO
+
+ALTER TABLE [dbo].[M_HelpContent] ADD  CONSTRAINT [DF_M_HelpContent_Status]  DEFAULT ((1)) FOR [Status]
+GO
+
+ALTER TABLE [dbo].[M_HelpContent] ADD  CONSTRAINT [DF_M_HelpContent_ClickNumber]  DEFAULT ((0)) FOR [ClickNumber]
+GO
+
+ALTER TABLE [dbo].[M_HelpContent] ADD  CONSTRAINT [DF_M_HelpContent_CreateTime]  DEFAULT (getdate()) FOR [CreateTime]
+GO
+
+ALTER TABLE [dbo].[M_HelpContent] ADD  CONSTRAINT [DF_M_HelpContent_UpdateTime]  DEFAULT (getdate()) FOR [UpdateTime]
+GO
 
 
 alter table orders add IsPublic int default(0)
