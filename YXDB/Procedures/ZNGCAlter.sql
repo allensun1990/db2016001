95178559fd0842a0bddb0c1f557b920daca8e023
sunallen521 <sunallen@163.com>
2016-09-27 18:05:27 +0800

    处理在线下单生产状态相关SP

--- YXDB/Procedures/ZNGCAlter.sql
+++ YXDB/Procedures/ZNGCAlter.sql
@@ -1,3 +1,7 @@
 ﻿
-insert into Menu(MenuCode,Name,Area,Controller,[View],IcoPath,IcoHover,Type,IsHide,PCode,Sort,Layer,IsMenu,IsLimit,Remark) 
-values ('203010500','工量统计','','Orders','UserLoadReport','','',1,0,'102010000',4,3,1,1,'')
+--insert into Menu(MenuCode,Name,Area,Controller,[View],IcoPath,IcoHover,Type,IsHide,PCode,Sort,Layer,IsMenu,IsLimit,Remark) 
+--values ('203010500','工量统计','','Orders','UserLoadReport','','',1,0,'102010000',4,3,1,1,'')
+
+alter table StorageDoc add ProgressStatus int default 0
+GO
+Update StorageDoc set ProgressStatus=0
